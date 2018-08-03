12ab328cf3cd6eb51f0744eb379c9326f6c973da
Elijah <dostoevskylabs@users.noreply.github.com>
2018-08-03 16:08:45 -0400

    Create options.js

---
+++ options.js
@@ -0,0 +1,50 @@
+let savedScopes = [];
+
+function remove( scope ) {
+  let index = savedScopes.indexOf( scope );
+  if ( index !== -1 ) {
+    savedScopes.splice(index, 1);
+    chrome.storage.sync.set({
+      savedScopes
+    }, function() {
+      let status = document.getElementById('status');
+      status.textContent = 'Scope removed.';
+      setTimeout(function() {
+        window.location.reload();
+      }, 750);    
+    });    
+  }
+}
+
+function save_options () {
+  let scope       = document.getElementById('scope').value;
+  savedScopes.push(scope);
+  chrome.storage.sync.set({
+    savedScopes
+  }, function() {
+    let status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      window.location.reload();
+    }, 750);
+  });
+}
+
+function restore_options () {
+  chrome.storage.sync.get({
+    savedScopes: [],
+  }, function(item) {
+    for ( let i in item.savedScopes ) {
+      savedScopes.push ( item.savedScopes[i] );
+      document.getElementById('scopes').innerHTML += "<a href='#' id='remove-"+savedScopes[i]+"'>"+savedScopes[i]+"</a><br/>";
+    }
+    for ( let i = 0; i < savedScopes.length; i++ ) {
+      document.getElementById('remove-' + savedScopes[i]).addEventListener('click', function(evt){
+        remove(evt.target.id.split('-')[1]);
+      })
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
