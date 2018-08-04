4a1eb39a4921a8cbc37d56addeb0b9308ed7a002
Elijah <dostoevskylabs@users.noreply.github.com>
2018-08-04 17:46:39 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 let scope       = [];
 let currentHost = "";
 
-const getHost = ( url ) => ( url.indexOf("://") > -1 ? url.split("/")[2] : url.split("/")[0] ).split(":")[0].split("?")[0];
+const getHost = ( url ) => ( url.indexOf("://") > -1 ? url.split("/")[2] : url.split("/")[0].split(":")[0].split("?")[0] );
 
 chrome.tabs.onActivated.addListener(function(currentTab) {
 	chrome.storage.sync.get({
@@ -9,27 +9,26 @@ chrome.tabs.onActivated.addListener(function(currentTab) {
 	}, function(item) {
 		if ( item.savedScopes.length === 0 ) return 0;
 	  for ( let i in item.savedScopes ) {
-		scope.push( item.savedScopes[i] );
+			if ( !scope.includes( item.savedScopes[i] ) ) scope.push( item.savedScopes[i] );
 	  }
 	  chrome.tabs.get(currentTab.tabId, function(tab){
-		currentHost = getHost(tab.url);
-		const config = {
-		  mode: "fixed_servers",
-			rules: {
-			  proxyForHttp: {
-			  scheme: "http",
-			  host: "127.0.0.1",
-			  port: 8080
-			  }
-			}
-		};
+			currentHost = getHost(tab.url);
+			const config = {
+				mode: "fixed_servers",
+				rules: {
+					proxyForHttp: {
+						scheme: "http",
+						host: "127.0.0.1",
+						port: 8080
+					}
+				}
+			};
 
-		if ( scope.includes( currentHost ) ) {
-		  console.log(scope);
-		  chrome.proxy.settings.set({ value: config, scope: 'regular' }, function () {
-			// stuff
-		  });
-		}
+			if ( scope.includes( currentHost ) ) {
+				chrome.proxy.settings.set({ value: config, scope: 'regular' }, function () {
+				// stuff
+				});
+			}
 	  });	  
 	});
 });
