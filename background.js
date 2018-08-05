ca1709c1870ef207d34acb72f2f0af3cb63cb7b0
Elijah <dostoevskylabs@users.noreply.github.com>
2018-08-04 22:20:19 -0400

    Update background.js

--- background.js
+++ background.js
@@ -23,82 +23,80 @@ function handleRequest( requestData, sendResponse ) {
 
 chrome.extension.onRequest.addListener(
   function(request, sender, sendResponse) {
-		handleRequest( request, sendResponse );
+    handleRequest( request, sendResponse );
   }
 );
 
 function addScope( scope, sendResponse ) {
-	if ( !scopeArray.includes( getHost( scope ) ) ) {
-		scopeArray.push( getHost( scope ) );
-
+  if ( !scopeArray.includes( getHost( scope ) ) ) {
+    scopeArray.push( getHost( scope ) );
     chrome.storage.sync.set({
       savedScopes : scopeArray
     }, function() {
-			return sendResponse({
-				method 	: 'saveSuccess',
-				data 		: getHost( scope )
-			});
-		});
-	} else {
-		return sendResponse({
-			method 		: 'saveFailed',
-			data			: 'Scope exists.'
-		});
-	}
+      return sendResponse({
+        method 	: 'saveSuccess',
+        data 		: getHost( scope )
+      });
+    });
+  } else {
+    return sendResponse({
+      method 		: 'saveFailed',
+      data			: 'Scope exists.'
+    });
+  }
 }
 
 function removeScope( scope, sendResponse ) {
-	if ( !scopeArray.includes( getHost( scope ) ) ) {
-		return sendResponse({
-			method 	: 'deleteFailed',
-			data 		: 'Scope doesn\'t exist.'
-		});
-	} else {
-		let index = scopeArray.indexOf( scope );
-	
-		if ( index !== -1 ) {
-			scopeArray.splice( index, 1 );
-			chrome.storage.sync.set({
-				savedScopes : scopeArray
-			}, function() {
-				return sendResponse({
-					method 	: 'deleteSuccess',
-					data 		: getHost( scope )
-				});
-			});    
-		}
-	}
+  if ( !scopeArray.includes( getHost( scope ) ) ) {
+    return sendResponse({
+       method 	: 'deleteFailed',
+       data 		: 'Scope doesn\'t exist.'
+    });
+  } else {
+    let index = scopeArray.indexOf( scope );
+    if ( index !== -1 ) {
+      scopeArray.splice( index, 1 );
+      chrome.storage.sync.set({
+        savedScopes : scopeArray
+      }, function() {
+        return sendResponse({
+          method 	: 'deleteSuccess',
+          data 		: getHost( scope )
+        });
+      });    
+    }
+  }
 }
 
 chrome.storage.sync.get({
-	savedScopes: [],
+  savedScopes: [],
 }, function ( item ) {
-	if ( item.savedScopes.length === 0 ) return 0;
+  if ( item.savedScopes.length === 0 ) return 0;
 
-	for ( let i in item.savedScopes ) {
-		if ( !scopeArray.includes( getHost( item.savedScopes[i] ) ) ) scopeArray.push( getHost( item.savedScopes[i] ) )
-	}
+  for ( let i in item.savedScopes ) {
+    if ( !scopeArray.includes( getHost( item.savedScopes[i] ) ) ) scopeArray.push( getHost( item.savedScopes[i] ) )
+  }
 });
 
 const config = {
-	mode: "fixed_servers",
-	rules: {
-		proxyForHttp: {
-			scheme: "http",
-			host: "127.0.0.1",
-			port: 8080
-		}
-	}
+  mode: "fixed_servers",
+  rules: {
+    proxyForHttp: {
+      scheme: "http",
+      host: "127.0.0.1",
+      port: 8080
+    }
+  }
 };
 
 chrome.webRequest.onBeforeRequest.addListener( function ( details ) {
-	if ( scopeArray.includes( getHost( details.url ) ) ) {
+  if ( scopeArray.includes( getHost( details.url ) ) ) {
     chrome.proxy.settings.set({ value: config, scope: 'regular' }, function () {
-			// proxying
-		});
-
-		return { cancel: false };
-	}
-	chrome.proxy.settings.set({ value: { mode: 'direct' }, scope: 'regular' }, function() {});
-	return { cancel: false };
+      // proxying
+    });
+	  
+    return { cancel: false };
+  }
+  chrome.proxy.settings.set({ value: { mode: 'direct' }, scope: 'regular' }, function() {});
+  return { cancel: false };
 }, {urls: ["<all_urls>"]}, ["blocking"]);
