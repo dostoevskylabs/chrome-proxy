8ea9f9874111fa42ed95bce8ecd11a48395dc982
Elijah <dostoevskylabs@users.noreply.github.com>
2018-08-04 22:07:20 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,26 +1,87 @@
-let scope = [];
-let currentHost = "";
+let scopeArray 	= [];
+const getHost 	= ( url ) => {
+	if ( !url ) return false;
+	return url.indexOf("://") > -1 ? url.split("/")[2] : url.split("/")[0].split(":")[0].split("?")[0];
+};
 
-const getHost = ( url ) => ( url.indexOf("://") > -1 ? url.split("/")[2] : url.split("/")[0].split(":")[0].split("?")[0] );
+function handleRequest( requestData, sendResponse ) {
+	switch ( requestData.method ) {
+		case 'INIT':
+			sendResponse({
+				method 	: 'INIT',
+				data 		: scopeArray
+			});
+		break;
 
-chrome.tabs.onActivated.addListener(function(currentTab) {
-	chrome.storage.sync.get({
-	  savedScopes: [],
-	}, function(item) {
-		if ( item.savedScopes.length === 0 ) return 0;
-
-	  for ( let i in item.savedScopes ) {
-			if ( !scope.includes( item.savedScopes[i] ) ) scope.push( item.savedScopes[i] );
-		}
+		case 'SAVE':
+			addScope( requestData.data, sendResponse );
+		break;
 		
-	  chrome.tabs.get(currentTab.tabId, function(tab){
-			currentHost = getHost(tab.url);
-	  });	  
-	});
+		case 'DELETE':
+			removeScope( requestData.data, sendResponse );
+		break;
+	}
+}
+
+chrome.extension.onRequest.addListener(
+  function(request, sender, sendResponse) {
+		handleRequest( request, sendResponse );
+  }
+);
+
+function addScope( scope, sendResponse ) {
+	if ( !scopeArray.includes( getHost( scope ) ) ) {
+		scopeArray.push( getHost( scope ) );
+
+    chrome.storage.sync.set({
+      savedScopes : scopeArray
+    }, function() {
+			return sendResponse({
+				method 	: 'saveSuccess',
+				data 		: getHost( scope )
+			});
+		});
+	} else {
+		return sendResponse({
+			method 		: 'saveFailed',
+			data			: 'Scope exists.'
+		});
+	}
+}
+
+function removeScope( scope, sendResponse ) {
+	if ( !scopeArray.includes( getHost( scope ) ) ) {
+		return sendResponse({
+			method 	: 'deleteFailed',
+			data 		: 'Scope doesn\'t exist.'
+		});
+	} else {
+		let index = scopeArray.indexOf( scope );
+	
+		if ( index !== -1 ) {
+			scopeArray.splice( index, 1 );
+			chrome.storage.sync.set({
+				savedScopes : scopeArray
+			}, function() {
+				return sendResponse({
+					method 	: 'deleteSuccess',
+					data 		: getHost( scope )
+				});
+			});    
+		}
+	}
+}
+
+chrome.storage.sync.get({
+	savedScopes: [],
+}, function ( item ) {
+	if ( item.savedScopes.length === 0 ) return 0;
+
+	for ( let i in item.savedScopes ) {
+		if ( !scopeArray.includes( getHost( item.savedScopes[i] ) ) ) scopeArray.push( getHost( item.savedScopes[i] ) )
+	}
 });
 
-// needs to be reworked as we're using an array of scopes instead of a single scope now.
-// currently doesnt work.
 const config = {
 	mode: "fixed_servers",
 	rules: {
@@ -32,9 +93,14 @@ const config = {
 	}
 };
 
-if ( scope.includes( currentHost ) ) {
-	console.log(currentHost);
-	chrome.proxy.settings.set({ value: config, scope: 'regular' }, function () {
-	// stuff
-	});
-}
+chrome.webRequest.onBeforeRequest.addListener( function ( details ) {
+	if ( scopeArray.includes( getHost( details.url ) ) ) {
+		chrome.proxy.settings.set({ value: config, scope: 'regular' }, function () {
+			console.log('aye');
+		});
+
+		return { cancel: false };
+	}
+	chrome.proxy.settings.set({ value: { mode: 'direct' }, scope: 'regular' }, function() {});
+	return { cancel: false };
+}, {urls: ["<all_urls>"]}, ["blocking"]);
