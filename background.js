d98fb90c60f9ee16759a0a8f6a9c9477c10822dd
Elijah <dostoevskylabs@users.noreply.github.com>
2018-08-11 14:24:58 -0400

    Update background.js

--- background.js
+++ background.js
@@ -2,10 +2,10 @@ let scopedTabId = 0;
 
 // dat burp doe.
 const config = {
-  mode: "fixed_servers",
+  mode: 'fixed_servers',
   rules: {
     singleProxy: {
-      host: "127.0.0.1",
+      host: '127.0.0.1',
       port: 8080
     }
   }
@@ -15,8 +15,28 @@ function addTabToScope ( tabId ) {
   scopedTabId = tabId;
 }
 
-function removeTabFromScope( tabId ) {
+function removeTabFromScope ( tabId ) {
   scopedTabId = 0;
+}
+
+function osd ( tabId ) {
+  if ( scopedTabId === 0 ) {
+    chrome.notifications.create( Math.random().toString(36).substring(0, 12), {
+      type    : 'basic',
+      title   : `Stopped intercepting tab: ${tabId}`,
+      message : '',
+      iconUrl : 'icons/test-48.png',
+    }, function () {}
+    );
+  } else {
+    chrome.notifications.create( Math.random().toString(36).substring(0, 12), {
+      type    : 'basic',
+      title   : `Intercepting tab: ${tabId}`,
+      message : '',
+      iconUrl : 'icons/test-48.png',
+    }, function () {}
+    );
+  }
 }
 
 // dem hotkeys doe.
@@ -28,8 +48,10 @@ chrome.commands.onCommand.addListener(function(command) {
           let tabId = currentTab[0].id;
           scopedTabId === 0 ? addTabToScope( tabId ) : removeTabFromScope( tabId );          
           if ( scopedTabId !== 0 ) {
+            osd( tabId );
             chrome.proxy.settings.set({ value: config, scope: 'regular' }, function () {});
           } else {
+            osd( tabId );
             chrome.proxy.settings.clear({ scope: 'regular' }, function () {});
           }
         }
@@ -39,13 +61,15 @@ chrome.commands.onCommand.addListener(function(command) {
 });
 
 // triggers when switching tabs
-chrome.tabs.onActivated.addListener(function(currentTab) {
-  // check if current tab should be proxied
-  if ( currentTab.tabId === scopedTabId ) {
-    // enable proxy
-    chrome.proxy.settings.set({ value: config, scope: 'regular' }, function () {});
-  } else {
-    // disable proxy
-    chrome.proxy.settings.clear({ scope: 'regular' }, function () {});
+chrome.tabs.onActivated.addListener(
+  function ( currentTab ) {
+    // check if current tab should be proxied
+    if ( currentTab.tabId === scopedTabId ) {
+      // enable proxy
+      chrome.proxy.settings.set({ value: config, scope: 'regular' }, function () {});
+    } else {
+      // disable proxy
+      chrome.proxy.settings.clear({ scope: 'regular' }, function () {});
+    }
   }
-});
+);
